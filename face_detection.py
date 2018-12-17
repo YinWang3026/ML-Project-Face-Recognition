af1fc75bc8bde051e44a57a3f5c1bf0786e2f95f
Mohammad Rafi <mr4448@nyu.edu>
2018-12-17 17:40:22 -0500

    opencv video with NN

--- face_detection.py
+++ face_detection.py
@@ -1,18 +1,59 @@
 # import the necessary packages
 import numpy as np
+from PIL import Image
 import cv2
 import os
+import keras
+from keras import applications
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten, Dense
+
+from keras.models import model_from_json
+from keras_vggface.vggface import VGGFace
+
+from tensorflow.python.client import device_lib
 
 image_path = 'images'
+
+with open('model.json','r') as f:
+    json = f.read()
+loaded_model = model_from_json(json)
+loaded_model.load_weights("weights.h5", by_name=True)
+
+names = ['Mohammad', 'Yining', 'Stranger']
+ 
+# evaluate loaded model on test data
+loaded_model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
 
 def save_faces(cascade, imgname):
     img = cv2.imread(os.path.join(image_path, imgname))
     for i, face in enumerate(cascade.detectMultiScale(img)):
         x, y, w, h = face
-        sub_face = img[y:y + h, x:x + w]
-        cv2.imshow('sub_face', sub_face)
+
+        im = Image.open(os.path.join(image_path, imgname))
+        center_x = x+w/2
+        center_y = y+h/2
+        #create dimensions for face to be cropped
+        b_dim = min(max(w,h)*1.2,im.width, im.height)
+        box = (center_x-b_dim/2, center_y-b_dim/2, center_x+b_dim/2, center_y+b_dim/2)
+
+        crpim = im.crop(box).resize((64,64)) #crops to 64 x 64 image
+        imarr = np.array(crpim).astype(np.float32) #turns image into numpy array
+
+        imarr = np.expand_dims(imarr, axis=0)
+        out = loaded_model.predict(imarr) #predict who is in the image
+        print(out)
+        best_index = np.argmax(out, axis=1)[0] #figure out index of highest percentage
+        print(names[best_index])
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2) #draw rectangle around face
+        texty = y - 10 if y - 10 > 10 else y + 10 
+        cv2.putText(img, names[best_index], (x, texty),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+        cv2.imshow('Image', img)
         cv2.waitKey(0)
-        cv2.imwrite(os.path.join("faces", "{}_{}.jpg".format(imgname, i)), sub_face)
+        #cv2.imwrite(os.path.join("faces", "{}_{}.jpg".format(imgname, i)), sub_face)
 
 face_cascade = "haarcascade_frontalface_default.xml"
 cascade = cv2.CascadeClassifier(face_cascade)
